eb538d3c00824efee44ae7f9f8facb7373865f6e
Victor Preto <Victoraugustonunespreto@gmail.com>
2024-07-30 11:10:47 -0300

    Create perfil2.py

---
+++ perfil2.py
@@ -0,0 +1,24 @@
+<p align="left"> 
+    Tenho quase 30 anos, moro no estado de São Paulo e sou apaixonada por tecnologias e animes! Sou um grande curioso pelo universo das linguagens de programação e 
+    Tenho quase 30 anos, moro no estado de São Paulo e sou apaixonado por tecnologias e animes! Sou um grande curioso pelo universo das linguagens de programação e 
+    um temerário quando o assunto é resolver problemas complexos, e quanto mais eu busco soluções, mais eu quero aprender! :octocat:<br>
+  </p>
+  <div display="inline-block">
+   <p align="left">👨‍💻 Faço parte da equipe docente da escola <a href="https://www.sp.senac.br/">Senac</a>;</p>
+   <p align="left">📚 Um estudante assíduo de Java Script, Java e SQL;</p>
+   <p align="left">🎮 Adoro jogos e meu objetivo é um dia desenvolver um.</p>
+   <p align="left">⛩️ Animes fazem parte da minha rotina, quer falar sobre eles é comigo mesmo.</p>
+  </div>
+  <a href="https://www.instagram.com/vmansolelli" target="_blank"><img align="left" alt="Instagram" width="22px" src="https://github.com/Aakarsh-B/trying-repos/blob/master/insta.svg" />
+  <a href="https://www.linkedin.com/in/victor-christofoleti-mansolelli-6a0792102/" target="_blank"><img align="left" alt="LinkedIn" width="22px" src="https://github.com/Aakarsh-B/trying-repos/blob/master/linkedin.svg" />
+  </br>
+  ### 🖥️ Tecnologias e Ferramentas: 
+  <div>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" title = "HTML5"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" title = "CSS3"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" title = "JAVASCRIPT"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" title = "JAVA"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" title = "C3"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" title = "SQL"/>
+    <img width="40px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" title = "MYSQL"/>
+  </div>
